39dd15a9ac7ca6be3451eef01800a81c6e9c14ee
LorenitaTL <lore7096@gmail.com>
2020-08-16 20:21:44 -0500

    Updates

---
+++ precache-manifest.e293700ace3a825972a3814d2eca2695.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86eb6d2e73ea88dd7a5ed802a451c70e",
+    "url": "/react-deploy/index.html"
+  },
+  {
+    "revision": "b207f7d164ce975e6d76",
+    "url": "/react-deploy/static/css/main.75c0ba97.chunk.css"
+  },
+  {
+    "revision": "340aac5933c756125f93",
+    "url": "/react-deploy/static/js/2.a0c31166.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-deploy/static/js/2.a0c31166.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b207f7d164ce975e6d76",
+    "url": "/react-deploy/static/js/main.3c386040.chunk.js"
+  },
+  {
+    "revision": "9a6b4006b58ad85b50f6",
+    "url": "/react-deploy/static/js/runtime-main.3e11c617.js"
+  },
+  {
+    "revision": "ada41ada4c6181dc4fa565a9586e04ac",
+    "url": "/react-deploy/static/media/lore-pic.ada41ada.jpg"
+  }
+]);
